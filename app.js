399f1a342147f94bc5491c42060638e18f3318bc
rucka <miroslav.rucka@posam.sk>
2016-02-14 13:02:22 +0100

    first push

--- app.js
+++ app.js
@@ -1,26 +1,21 @@
-var sensorLib = require("node-dht-sensor");
+var sensorLib = require('node-dht-sensor');
 
 var sensor = {
-    sensors: [ {
-        name: "Indoor",
-        type: 11,
-        pin: 25
-    }, {
-        name: "Outdoor",
-        type: 11,
-        pin: 4
-    } ],
-    read: function() {
-        for (var a in this.sensors) {
-            var b = sensorLib.readSpec(this.sensors[a].type, this.sensors[a].pin);
-            console.log(this.sensors[a].name + ": " +
-                b.temperature.toFixed(1) + "C, " +
-                b.humidity.toFixed(1) + "%");
-        }
-        setTimeout(function() {
+    initialize: function () {
+        return sensorLib.initialize(11, 17);
+    },
+    read: function () {
+        var readout = sensorLib.read();
+        console.log('Temperature: ' + readout.temperature.toFixed(2) + 'C, ' +
+            'humidity: ' + readout.humidity.toFixed(2) + '%');
+        setTimeout(function () {
             sensor.read();
         }, 2000);
     }
 };
 
-sensor.read();
+if (sensor.initialize()) {
+    sensor.read();
+} else {
+    console.warn('Failed to initialize sensor');
+}
