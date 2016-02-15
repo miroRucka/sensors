98ca8736fbb16caee4ed7bec0b306a0f312a9acd
rucka <miroslav.rucka@posam.sk>
2016-02-15 20:33:37 +0100

    sensor project - send sensor to server

---
+++ scripts/startSensors.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+PATH="/usr/local/bin:$PATH"
+sudo modprobe wire
+sudo modprobe w1-gpio
+sudo modprobe w1-therm
+cd /home/pi/github/sensors
+forever stopall
+sudo forever start -l /home/pi/logs/output.log -a app.js
+
