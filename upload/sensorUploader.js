0f718c87fece2b6c8c1fc74d8fb7c1f714f786be
rucka <miroslav.rucka@posam.sk>
2016-02-16 18:46:21 +0100

    sensor project - send sensor to server

--- upload/sensorUploader.js
+++ upload/sensorUploader.js
@@ -9,17 +9,23 @@ var headers = require('./headers');
 var Promise = require("promise");
 
 module.exports = function (sensorData) {
+
+    var _getEndpoint = function () {
+        var endpoint = process.argv[2];
+        return endpoint || config.serverEndpoint;
+    };
+
     return new Promise(function (resolve, reject) {
-        restler.postJson(config.serverEndpoint + "api/sensors", sensorData, {
+        restler.postJson(_getEndpoint() + "api/sensors", sensorData, {
             headers: headers
         }).on("success", function (data) {
-               resolve(data);
-            }).on("fail", function(err){
-                reject(err);
-                logger.error("exception sensor uploading", err);
-            }).on("error", function(err){
-                reject(err);
-                logger.error("exception sensor uploading", err);
-            });
+            resolve(data);
+        }).on("fail", function (err) {
+            reject(err);
+            logger.error("exception sensor uploading", err);
+        }).on("error", function (err) {
+            reject(err);
+            logger.error("exception sensor uploading", err);
+        });
     });
 };
