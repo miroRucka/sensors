b877816ed0d6d09f5aba09d24068346f4c0df0a7
rucka <miroslav.rucka@posam.sk>
2017-09-07 21:47:35 +0200

    sensor project - send sensor to server

--- messaging/photoSubscriber.js
+++ messaging/photoSubscriber.js
@@ -9,7 +9,7 @@ module.exports = function (stompClient) {
         logger.info('subscribe to ', destination);
         stompClient.subscribe(destination, function (body, headers) {
             var message = body ? body.pointId : 'empty';
-            logger.info('>> taking picture...', message);
+            logger.info('>> taking picture...', message, body, JSON.parse(body).pointId);
             cam().takePicture().then(function () {
                 logger.info('<< taking picture');
             });
