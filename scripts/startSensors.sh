9e9f19f2378ca551aec9c0beee6f5facd43dbb5b
Miroslav Rúčka <miro.rucka@gmail.com>
2018-05-06 13:56:49 +0000

    new version for new nodejs

--- scripts/startSensors.sh
+++ scripts/startSensors.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 PATH="/usr/local/bin:$PATH"
+
+echo ">> starting sensors app..."
+
 sudo modprobe wire
 sudo modprobe w1-gpio
 sudo modprobe w1-therm
 cd /home/pi/github/sensors
-sudo forever stopall
-sudo forever start -l /home/pi/logs/output.log -a app.js http://horske.info/
-#sudo forever start -l /home/pi/logs/output.log -a app.js
+sudo forever stop sensors.js
+sudo forever start -l /home/pi/logs/output.log -p /home/pi/github/sensors -a -d sensors.js http://horske.info/
+
+echo ">> running"
 
